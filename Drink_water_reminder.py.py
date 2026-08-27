51dc1dd8a5f5300b52c1b5b85671605931cee58f
Karan Zende <karanzende1376@gmail.com>
2026-08-27 07:56:46 +0530

    add comment

--- Drink_water_reminder.py.py
+++ Drink_water_reminder.py.py
@@ -1,13 +1,15 @@
 import time
 from plyer import notification
 
+# creat a function for the water reminder 
 def water_reminder():
-    while True:
+    while True: # Keep running the reminder continuously 
         notification.notify(
             title="water reminder for karan",
             message="Drink to sip some water",
             timeout=10
         )
-        time.sleep(5)
+        time.sleep(5) # Wait for 5 seconds before showing the notification again 
+    
 
 water_reminder()
